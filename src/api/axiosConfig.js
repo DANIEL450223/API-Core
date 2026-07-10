84c48b2bdca4cb5c5201cfe9f3a8e2439a4f6016
Daniel <emil.encalada@udla.edu.ec>
2026-07-09 19:40:28 -0500

    Corregir conexion del frontend con API Render

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  timeout: 15000,
+    baseURL:
+        import.meta.env.VITE_API_URL ||
+        "https://proyecto-calculo-degradacion-core.onrender.com",
+    timeout: 90000,
 });
 
 export default api;
